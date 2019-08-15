a8e747ba40beef3209714cab0c8d054c492da876
Paschalis Natsidis <pnatsidis@hotmail.com>
2019-08-15 16:24:46 +0100

    Update jackknife.py

--- jackknife.py
+++ jackknife.py
@@ -1,7 +1,7 @@
 '''
 This script creates jackknifed sets from a set of orthogroups, based on a given proportion.
 
-python jackknife.py input_filename
+python jackknife.py input_filename 100 0.7
 
 input_filename will contain the orthogroup filenames
 Here 100 jackknife replicates are created with each containing a random 70% of the original orthogroups
@@ -10,20 +10,17 @@ These 100 files can then used to retrieve the sequences from the corresponding o
 
 import numpy as np
 
-input_filename = sys.argv[-1]
+input_filename = sys.argv[-3]
+jackknife_replicates = sys.argv[-2]           # HOW MANY JACKKNIFE REPLICATES WILL BE CREATED   
+percentage_included = sys.argv[-1]            # WHAT PERCENTAGE OF THE TOTAL ORTHOGROUPS WILL BE IN EVERY JACKKNIFE REPLICATE 
 
-f = open(input_filename, "r")        # A FILE WITH ALL ORTHOGROUP FILENAMES CREATED WITH "ls OG* > allfiles.txt"
+f = open(input_filename, "r")                 # A FILE WITH ALL ORTHOGROUP FILENAMES CREATED WITH "ls OG* > allfiles.txt"
 lines = f.readlines()
 filenames = [x.strip() for x in lines]
 
-jackknife_replicates = 100           # HOW MANY JACKKNIFE REPLICATES WILL BE CREATED   
-percentage_included = 0.7            # WHAT PERCENTAGE OF THE TOTAL ORTHOGROUPS WILL BE IN EVERY JACKKNIFE REPLICATE  
-
-
-for i in range(0,jackknife_replicates): 
-    portion = np.random.choice(filenames, int(percentage_included*len(filenames)), replace = False)
-    print(portion[:10]) 
+for i in range(0, jackknife_replicates): 
+    random_subset = np.random.choice(filenames, int(percentage_included * len(filenames)), replace = False)
     output = open("jackknife_orthofinder_" + str(i) + ".txt", "w")       # CREATES A FILE FOR EVERY ITERATION WITH ORTHOGROUP FILENAMES IN IT
-    for entry in portion:
+    for entry in random_subset:
         output.write(entry + "\n")
     output.close()
