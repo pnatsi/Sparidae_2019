564c21914314d15f6a18a516a898744934d3c16e
Paschalis Natsidis <pnatsidis@hotmail.com>
2019-08-20 13:53:43 +0100

    Update get_longest_isoform.py

--- get_longest_isoform.py
+++ get_longest_isoform.py
@@ -1,4 +1,29 @@
-import sys
+"""                                                                                                                                                                                                                                           
+This script parses the output of 'get_longest_orf.py', which contains the longest predicted open reading frames (ORFs) per transcript.
+These ORFs may be different isoforms of the same gene, so we are interested in keeping the longest isoform per gene.
+The script can be run as follows:                                                                                                                                                                                                             
+                                                                                                                                                                                                                                              
+python get_longest_isoform.py input_filename output_filename header_prefix                                                                                                                                                                                     
+                                                                                                                                                                                                                                              
+The result will be in FASTA format and named 'output_filename'. It will contain only the longest predicted isoform per gene                                                                                                              
+                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                              
+For example:                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                              
+python get_longest_isoform.py Pagrus_longest_orfs.fasta Pagrus_longest_isoforms.fasta PAGR                                                                                                                                                                  
+                                                                                                                                                                                                                                              
+will output the file "Pagrus_longest_isoforms.fasta" with sequence headers:
+
+>PAGR_...
+...
+>PAGR_...
+...
+
+"""
+
+
+#!/usr/bin/pythonimport sys
 import re
 
 infilename = sys.argv[-3]
